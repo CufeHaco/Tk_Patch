0b819a4992274cd97aee6303d49b08916e71c24a
CufeHaco <crazyrabbitking89@gmail.com>
2019-05-09 00:04:58 -0400

    Add files via upload

---
+++ Rubytk_patch.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+#----------------------------------------------------------------------
+#-RubyTK patch to fix RubyTk for versions 2.4.0/2.6.0
+#-rubytk was removed from the compiler in 2.4.0.
+#-This patch is to fix the compatibility issue that keeps the TK gem
+#-from installing.  It will also install the TK gem.
+#-After running this Script, RubyTk should be working again.
+#-After alot of heart ache and research, I've created this script to
+#-keep others from having to deal with what I had to get it working again.
+#-this works with the tcl8.6-dev
+#----------------------MAY 08, 2019-------------------------------------
+#This script was written by Cufe_Haco.  Information found on
+#https://stackoverflow.com/questions/54293774/ruby-graphical-user-interface-tk-on-linux-ubuntu-dont-work
+#-----------------------------------------------------------------------
+
+class TK_Patch
+puts Time.now
+puts "Installing Ruby-All-dev"
+sleep(1)
+`sudo apt-get install ruby-all-dev`.chomp
+sleep(1)
+puts "Installing TK GEM with TCL8.6-dev"
+sleep(1)
+`sudo gem install tk -- --with-tcltkversion=8.6 \
+--with-tcl-lib=/usr/lib/x86_64-linux-gnu \
+--with-tk-lib=/usr/lib/x86_64-linux-gnu \
+--with-tcl-include=/usr/include/tcl8.6 \
+--with-tk-include=/usr/include/tcl8.6 \
+--enable-pthread`.chomp
+
+puts "testing TK GEM"
+sleep(1)
+puts Time.now
+require 'tk'
+
+root = TkRoot.new {title "TK Test!!!!"}
+root['geometry'] = '400x200'
+label = TkLabel.new {text "Tk test label!" ; pack}
+button = TkButton.new {text "EXIT" ; command {proc exit} ; pack}
+Tk.mainloop
+sleep(1)
+puts Time.now
+exit(0)
+end
+
+Tk_Patch.new
